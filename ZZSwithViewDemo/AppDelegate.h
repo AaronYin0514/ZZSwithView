87b51a5c1fa5378a12adeb91869f9e67c8d6e12d
AaronYin0514 <562540603@qq.com>
2016-03-16 09:03:19 +0800

    Initial Commit

---
+++ ZZSwithViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZSwithViewDemo
+//
+//  Created by 尹中正(外包) on 16/3/16.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
